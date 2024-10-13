42d0a211399ee7de658660443a387f6f64aefdc7
GitHub Action <action@github.com>
2024-10-13 18:08:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/sd1337/apexandordsforjetbrains/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sd1337/apexandordsforjetbrains/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.sd1337.apexandordsforjetbrains.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sd1337.apexandordsforjetbrains.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
